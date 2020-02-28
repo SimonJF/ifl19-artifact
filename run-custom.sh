b841bc61481784449da9a8589ef2de9ca329aba2
Rudi Horn <dyn-git@rudi-horn.de>
2020-02-28 08:37:23 +0000

    initial attempt at separating docker images

--- run-custom.sh
+++ run-custom.sh
@@ -3,4 +3,4 @@ if [ -z "$LINKS_PORT" ]
 then
   LINKS_PORT=8080
 fi
-docker run -ti -p $LINKS_PORT:8080 -v `pwd`/custom-examples:/home/opam/custom-examples --rm ifl19_links ./links/links --config=config custom-examples/$@
+docker run -ti -p $LINKS_PORT:8080 -v `pwd`/custom-examples:/home/opam/custom-examples --rm ifl19_links_gtopdb ./links/links --config=config custom-examples/$@
--- scripts/build-links.sh
+++ scripts/build-links.sh
@@ -3,3 +3,7 @@
 if [ ! "$(docker images ifl19_links | grep ifl19_links )" ]; then
 	docker build -t ifl19_links links-docker
 fi
+
+if [ ! "$(docker images ifl19_links_gtopdb | grep ifl19_links_gtopdb )" ]; then
+	docker build -t ifl19_links_gtopdb gtopdb-docker
+fi
